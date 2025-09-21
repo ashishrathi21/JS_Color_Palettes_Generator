06533d0bb4b24fe2270ee22ec83610d070bb7c99
Ashish Rathi <rathiashish4321@gmail.com>
2025-09-21 16:31:51 +0530

    color palette generator

---
+++ script.js
@@ -0,0 +1,40 @@
+let generateBtn = document.querySelector(".btn");
+let palette = document.querySelector(".palette");
+
+function generatePalette() {
+  const colors = [];
+
+  for (let i = 0; i < 5; i++) {
+    colors.push(generateRandomColors());
+  }
+
+  updatePaletteColors(colors);
+}
+
+function generateRandomColors() {
+  const letters = "ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+
+  return color;
+}
+
+function updatePaletteColors(colors) {
+  const colorBoxes = document.querySelectorAll(".color-wrapper");
+
+  colorBoxes.forEach((box, index) => {
+    const color = colors[index];
+    const colorDiv = box.querySelector(".color-box");
+    const hexValue = box.querySelector(".hex-code");
+
+    colorDiv.style.backgroundColor = color;
+    hexValue.textContent = color;
+  });
+}
+
+generateBtn.addEventListener("click", generatePalette);
+
+generatePalette();
